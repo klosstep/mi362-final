9e83aec558a8eb4d097013fa181b7865d79e7039
klosstep <klosstep@msu.edu>
2016-05-02 21:06:08 -0400

    updated app

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
-<?php 
-  echo "This is a test file."
-?>
+<!doctype html>
+<html class="no-js" lang="">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Welcome</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script>
+      $(function() {
+        $('.center').fadeIn(2000).removeClass('hidden');
+      });
+    </script>
+  </head>
+
+  <body>
+    <div class="container">
+      <div class="center hidden">
+        <div id="text">
+          <p>Click Below To See Project</p>
+          <p><a id="link" target="_blank" href="http://www.stephenklosweb.com/to-do/index.php">Go To App</a></p>
+        </div>
+        </p>
+      </div>
+    </div>
+  </body>
+</html>
